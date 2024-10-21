0757cbe7b9f8c8cab24c9ed6f5a9bbca22a85e44
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2024-10-21 17:37:05 +0200

    estructura base

---
+++ src/main/VentanaPrincipal.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.SwingUtilities;
+import gui.VentanaInicioSesion;
+
+public class VentanaPrincipal {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaInicioSesion();
+				
+			}
+		});
+		
+	}
+}
