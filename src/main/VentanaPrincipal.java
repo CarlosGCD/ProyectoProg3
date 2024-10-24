e7711ae3e7696fc409c2c70b347b912d32e6c660
CarlosGCD <145966685+CarlosGCD@users.noreply.github.com>
2024-10-24 16:11:36 +0200

    prueba

--- src/main/VentanaPrincipal.java
+++ src/main/VentanaPrincipal.java
@@ -6,7 +6,7 @@ import gui.VentanaInicioSesion;
 public class VentanaPrincipal {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-			
+			//hola
 			@Override
 			public void run() {
 				new VentanaInicioSesion();
