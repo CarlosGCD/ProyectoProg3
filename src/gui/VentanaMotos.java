c62d6897d594ce48b088d344e42087fc6d384771
Iñigo Garcia <inigo.g.p@opendeusto.es>
2024-10-28 19:18:18 +0100

    Creación de la Ventana de Motos

---
+++ src/gui/VentanaMotos.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class VentanaMotos extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	public VentanaMotos() {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		this.setTitle("Inventario de Motos");
+		this.setSize(400, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+}
