b685fcaf35a9d00e3211ce525cb05f2609956778
Aitor Cornejo <aitor.c.s@opendeusto.es>
2024-11-02 19:01:47 +0100

    Mejoras en el menu

--- src/gui/VentanaMenu.java
+++ src/gui/VentanaMenu.java
@@ -1,9 +1,14 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
 
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,7 +24,7 @@ private static final long serialVersionUID = 1L;
 	protected JPanel pNorte,pSur,pCentro;
 	protected JButton btnMotos, btnMotoSegundaMano, btnVolver;
 	protected JLabel lbienvenida; 
-	
+	private BufferedImage fondo;
 	
 	public VentanaMenu(JFrame vAnterior) {
 		super();
@@ -32,19 +37,41 @@ private static final long serialVersionUID = 1L;
 		setTitle("Menu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		//Cargar la imagen para el fondo
+		try {
+            fondo = ImageIO.read(new File("resources/images/concesionarioFondo.png")); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		//Crear un JPanel personalizado
+		JPanel pFondo = new JPanel() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (fondo != null) {
+                    g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
+                } else {
+                	System.out.println("Error: La imagen de fondo no se ha cargado.");
+                }
+            }
+        };
+        
 		//Creación de los paneles y los añadimos a la ventana
 		pCentro = new JPanel();
 		pSur = new JPanel();
 		pNorte = new JPanel();
 		
-		pCentro.setLayout(new GridLayout(2, 1, 20, 20));
-		pCentro.setBorder(new EmptyBorder(170, 250, 170, 250)); //Arriba, izq, abajo, drch
+		pCentro.setOpaque(false);
+		pSur.setOpaque(false);
+		pNorte.setOpaque(false);
 		
-		pNorte.setBorder(new EmptyBorder(20, 0, 0, 0));
+		pCentro.setLayout(new GridLayout(3, 1, 20, 20));
+		pCentro.setBorder(new EmptyBorder(150, 100, 100, 100)); //Arriba, izq, abajo, drch
 		
-		getContentPane().add(pCentro, BorderLayout.CENTER);
-		getContentPane().add(pSur, BorderLayout.SOUTH);
-		getContentPane().add(pNorte, BorderLayout.NORTH);
+		pNorte.setBorder(new EmptyBorder(15, 0, 0, 0));
 		
 		//Creamos los botones 
 		btnMotos = new JButton("Visualizar Motos");
@@ -52,14 +79,20 @@ private static final long serialVersionUID = 1L;
 		
 		btnMotoSegundaMano = new JButton("Visualizar Motos de Segunda Mano");
 		pCentro.add(btnMotoSegundaMano);
+		
 		btnVolver = new JButton("Volver");
-		pSur.add(btnVolver);
+		pCentro.add(btnVolver);
         
 		//Creamos el mensaje de bienvenida
 		lbienvenida = new JLabel("¡Bienvenid@ a nuestro concesionario de motos!");
-		Font fuente = new Font("Times New Roman", Font.BOLD, 20); 
+		Font fuente = new Font("Times New Roman", Font.BOLD, 24); 
 		lbienvenida.setFont(fuente);
 		pNorte.add(lbienvenida);
+		
+		//Añadir los paneles al panel principal
+        pFondo.add(pNorte, BorderLayout.NORTH);
+        pFondo.add(pCentro, BorderLayout.CENTER);
+        pFondo.add(pSur, BorderLayout.SOUTH);
 		
 		//ActionListeners:
 		btnMotos.addActionListener((e)-> {
@@ -72,8 +105,9 @@ private static final long serialVersionUID = 1L;
 			vActual.dispose();
 			vAnterior.setVisible(true);
 		});
-		
+		add(pFondo);
 		
 		setVisible(true);
 	}
+	
 }
