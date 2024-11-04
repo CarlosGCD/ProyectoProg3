7775f7ba1fd4aa392954f94247fefa53c398ea83
Mikel <mikel.g.matos@opendeusto.es>
2024-11-04 11:38:31 +0100

    creacion de la clase peronsa y eliminacion de usuario y trabajador

--- src/domain/Persona.java
+++ src/domain/Persona.java
@@ -1,44 +1,45 @@
 package domain;
 
-public class Trabajador {
+public class Persona {
 	public int cod;
 	public String nombre;
 	public String apellidos;
-	
-	public Trabajador(int cod, String nombre, String apellidos) {
+	public boolean trabajador;
+	public Persona(int cod, String nombre, String apellidos, boolean trabajador) {
 		super();
 		this.cod = cod;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
+		this.trabajador = trabajador;
 	}
-
 	public int getCod() {
 		return cod;
 	}
-
 	public void setCod(int cod) {
 		this.cod = cod;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getApellidos() {
 		return apellidos;
 	}
-
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-
+	public boolean isTrabajador() {
+		return trabajador;
+	}
+	public void setTrabajador(boolean trabajador) {
+		this.trabajador = trabajador;
+	}
 	@Override
 	public String toString() {
-		return "Trabajador [cod=" + cod + ", nombre=" + nombre + ", apellidos=" + apellidos + "]";
+		return "Persona [cod=" + cod + ", nombre=" + nombre + ", apellidos=" + apellidos + ", trabajador=" + trabajador
+				+ "]";
 	}
 	
 	
